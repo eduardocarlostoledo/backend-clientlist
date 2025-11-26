7823c27d2ababa249a2eaa062012c261a5e49c3d
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2025-11-26 12:44:32 -0300

    fixa da sender

--- subscription-email-app/src/services/emailService.js
+++ subscription-email-app/src/services/emailService.js
@@ -9,12 +9,10 @@ dotenv.config();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const transporter = nodemailer.createTransport({
-    // host: process.env.EMAIL_HOST,
-    // port: process.env.EMAIL_PORT,
-    // secure: false,
-    host: 'smtp.gmail.com',
-    port: 465,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     secure: true,
+        
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
